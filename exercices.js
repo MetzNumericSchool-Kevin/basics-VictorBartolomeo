6ea4704804a8490739ff4b58229e1c714e7aefd4
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-02-06 22:16:59 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -1,6 +1,13 @@
 //Définition de variables
 
-const storeName="Valinor";
-let stockHealthPotion=10;
-let costHealtPotion=50
-let isStoreOpen=false;
+const storeName = "Valinor";
+let stockHealthPotion = 10;
+let costHealtPotion = 50
+let isStoreOpen = false;
+
+//Affichage conditionnel
+if (isStoreOpen)
+console.log("Bienvenue dans la boutique \"" + storeName + "\" Aventurier ! 🎉");
+else
+console.log("La boutique \""+ storeName+ "\" est fermée, revenez plus tard Aventurier ! 😴");
+
